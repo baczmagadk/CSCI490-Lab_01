cf46cb476e3786274bb7ec56e913f05b1f44049a
Daniel Baczmaga <baczmagadk@g.cofc.edu>
2019-01-19 08:33:15 -0500

    Implement OnClickListener

--- app/src/main/java/com/example/simpletimer/MainActivity.java
+++ app/src/main/java/com/example/simpletimer/MainActivity.java
@@ -2,12 +2,20 @@ package com.example.simpletimer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
