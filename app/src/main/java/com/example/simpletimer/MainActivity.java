414851d06978b264c4f34d28c7ec034dad222852
Daniel Baczmaga <baczmagadk@g.cofc.edu>
2019-01-19 08:56:26 -0500

    Create Subclass CountDownTimer

--- app/src/main/java/com/example/simpletimer/MainActivity.java
+++ app/src/main/java/com/example/simpletimer/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.simpletimer;
 
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import java.lang.Object;
 
 public class MainActivity extends AppCompatActivity implements OnClickListener {
 
@@ -13,9 +15,18 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
         setContentView(R.layout.activity_main);
     }
 
-
     @Override
     public void onClick(View v) {
+
+    }
+
+    public abstract class CountDownTimer extends Object {
+
+        public CountDownTimer() {
+        }
+
+        public CountDownTimer(long millisInFuture, long countDownInterval) {
+        }
 
     }
 }
